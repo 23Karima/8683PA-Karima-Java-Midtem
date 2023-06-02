8b1cbf89666427a695b8f2548558b801defc713d
23Karima <karima21ait@gmail.com>
2023-06-02 18:36:15 -0400

    Add input string and test the program

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -19,5 +19,29 @@ public class Permutation {
             System.out.println(permutation);
         }
     }
+    public static List<String> computePermutations(String input) {
+        List<String> permutations = new ArrayList<>();
+        computePermutationsHelper(input.toCharArray(), 0, permutations);
+        return permutations;
+    }
+
+    private static void computePermutationsHelper(char[] inputArray, int currentIndex, List<String> permutations) {
+        if (currentIndex == inputArray.length - 1) {
+            permutations.add(String.valueOf(inputArray));
+        } else {
+            for (int i = currentIndex; i < inputArray.length; i++) {
+                swap(inputArray, currentIndex, i);
+                computePermutationsHelper(inputArray, currentIndex + 1, permutations);
+                swap(inputArray, currentIndex, i);
+            }
+        }
+    }
+
+    private static void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
 
 
