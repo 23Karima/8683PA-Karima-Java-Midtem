7f102fb7571c283d2add83cb5ca35fe856ca1ea7
23Karima <karima21ait@gmail.com>
2023-06-01 23:18:19 -0400

    DuplicateWord program complete.

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,6 @@
 package string_problems;
-
+import java.util.HashMap;
+import java.util.Map;
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -8,10 +9,46 @@ public class DuplicateWord {
      */
 
     public static void main(String[] args) {
+        // given the sentence.
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
-        // Implement here
+        //first remove punctuation marks from the sentence using the replaceAll() method with a regular expression pattern [^a-zA-Z ]
+        // and convert the sentence to lowercase using the toLowerCase() method
+        st = st.replaceAll("[^a-zA-Z ]", "").toLowerCase();
 
+        // Split the sentence into words,
+        String[] words = st.split(" ");//here,the space character " " is used as the delimiter to separate the sentence into individual words.
+
+        // Create a map to store words and their occurrences
+        Map<String, Integer> wordOccurrences = new HashMap<>();
+
+        // Iterate over the words and count their occurrences.
+        for (String word : words) {
+            if (wordOccurrences.containsKey(word)) {
+                wordOccurrences.put(word, wordOccurrences.get(word) + 1);
+            } else {
+                wordOccurrences.put(word, 1);
+            }
+        }
+        // Print duplicate words and their occurrences
+        System.out.println("Duplicate words and their occurrences:");
+        for (Map.Entry<String, Integer> entry : wordOccurrences.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+        // Calculate the average length of all words
+        int totalLength = 0;//declare an integer variable totalLength and initialize it to 0.
+        for (String word : words) {
+            totalLength += word.length();
+        }
+        //we divide the totalLength by the number of words
+        double averageLength = (double) totalLength / words.length;
+        //print the calculated average length of words
+        System.out.println("Average length of words: " + averageLength);
     }
-
 }
+
+
+
+
