f3a3ebe858752c3e15bd55e42abd80a37e5751b1
23Karima <karima21ait@gmail.com>
2023-06-02 00:25:00 -0400

    Palindrome complete.

--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -12,8 +12,34 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
+            String word = "MADAM"; // Change this to test different words
+
+            if (isPalindrome(word)) {
+                System.out.println(word + " is a palindrome.");
+            } else {
+                System.out.println(word + " is not a palindrome.");
+            }
+        }
+
+        public static boolean isPalindrome(String word) {
+            int left = 0;
+            int right = word.length() - 1;
+
+            while (left < right) {
+                if (word.charAt(left) != word.charAt(right)) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
     }
 
-    // Implement here
 
-}
+
+
+
+
