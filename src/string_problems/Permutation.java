681e6d9574f22754411c63cc049ecd95f0e82a59
23Karima <karima21ait@gmail.com>
2023-06-02 18:27:15 -0400

    Initial commit with the Permutation class

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -1,15 +1,23 @@
 package string_problems;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Permutation {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Create a Java program to compute all permutations of any given string
-     *
+     * <p>
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
     public static void main(String[] args) {
+        String input = "ABC";
+        List<String> permutations = computePermutations(input);
 
+        // Print the computed permutations
+        for (String permutation : permutations) {
+            System.out.println(permutation);
+        }
     }
-    // Implement Here
-}
+
+
