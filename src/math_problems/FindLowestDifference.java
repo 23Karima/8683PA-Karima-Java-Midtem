dcc7fd7fd4a2ae7729ef2e8ab0d1c04f36ecbe13
23Karima <karima21ait@gmail.com>
2023-06-03 13:58:24 -0400

    This program finds the lowest number that is not shared between two arrays.

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,5 +1,6 @@
 package math_problems;
 
+import java.util.HashSet;
 public class FindLowestDifference {
 
     /** INSTRUCTIONS
@@ -8,13 +9,36 @@ public class FindLowestDifference {
      */
 
     public static void main(String[] args) {
-
-
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
-
+        int lowestNumber = findLowestDifference(array1, array2);
+        System.out.println("The lowest number not shared between the arrays is: " + lowestNumber);
     }
 
+    public static int findLowestDifference(int[] array1, int[] array2) {
+        HashSet<Integer> set = new HashSet<>();
 
+        // Add all elements from array1 to the set
+        for (int num : array1) {
+            set.add(num);
+        }
 
+        // Remove elements from array2 that are already in the set
+        for (int num : array2) {
+            if (set.contains(num)) {
+                set.remove(num);
+            }
+        }
+
+        // Find the minimum number in the set
+        int lowestNumber = Integer.MAX_VALUE;
+        for (int num : set) {
+            if (num < lowestNumber) {
+                lowestNumber = num;
+            }
+        }
+
+        return lowestNumber;
+    }
 }
+
