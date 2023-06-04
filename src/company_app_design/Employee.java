f31e683f563ec1d3cd4a0d089fcfebe1758c0f11
23Karima <karima21ait@gmail.com>
2023-06-04 03:48:03 -0400

    interface that have a set of methods related to employee information.

--- src/company_app_design/Employee.java
+++ src/company_app_design/Employee.java
@@ -1,27 +1,23 @@
 package company_app_design;
 
 public interface Employee {
-	
-	/** INSTRUCTIONS
-     *
+
+    /** INSTRUCTIONS
      * Employee is an Interface which contains multiple unimplemented methods.
-     *
      * Read the methods and understand what they might be used for. What could be the requirements behind creating such
      * methods? Think about it, and then implement them in a concrete class.
-     *
      * You also need to add some additional methods to meet any additional business requirements of this application
-     *
      * Get creative - think like a dev
-	 */
+     */
 
     int employeeId();
 
     String employeeName();
 
-    void assignDepartment();
+    String assignDepartment();
 
     int calculateSalary();
 
-    void benefits();
+    double benefits();
 
 }
