7fc44fb1dd6a940c7eec02aa03c6d6ede489bfb7
23Karima <karima21ait@gmail.com>
2023-06-04 03:06:27 -0400

    The program generates and prints an array at least 40 of Fibonacci numbers.

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -4,7 +4,7 @@ public class Fibonacci {
 
     /**
      * INSTRUCTIONS
-     * Write a method that will print or return at least 40 Fibonacci number
+     * Write a method that will print or return at least 40 Fibonacci number.
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
