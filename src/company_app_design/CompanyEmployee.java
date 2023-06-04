fbc1d2786d314ebce0b9f574be856d81a5db53ea
23Karima <karima21ait@gmail.com>
2023-06-04 03:44:45 -0400

    demonstrates various functionalities related to employee info, such as addresses, calculating bonuses and salaries, retrieving employee details, and performing other operations.

--- src/company_app_design/CompanyEmployee.java
+++ src/company_app_design/CompanyEmployee.java
@@ -13,13 +13,54 @@ public class CompanyEmployee {
      *  Try to think like a Software Developer
      */
     public static void main(String[] args) {
+        // Create an instance of EmployeeInfo
+        EmployeeInfo employeeInfo = new EmployeeInfo(1254874521, "7117 Sanlin dr");
+        System.out.println("Employee address: " + employeeInfo.getEmployeeAddress());
+        employeeInfo.setEmployeeAddress("1920 Solly Ave");// update employee address.
+        System.out.println("Updated employee address: " + employeeInfo.getEmployeeAddress());
 
+        // Calculate annual bonus
+        double annualBonus = EmployeeInfo.calculateAnnualBonus(4000, 3);
+        System.out.println("Annual bonus: " + annualBonus);
 
+        // Create an instance of EmployeeInfo of name, ID, and gross salary
+        EmployeeInfo emp1 = new EmployeeInfo("Karima", 300500, 40000.00);
+        System.out.println("Employee name: " + emp1.getEmployeeName());
+        System.out.println("Employee ID: " + emp1.getEmployeeId());
+        System.out.println("Employee gross salary: " + emp1.getGrossSalary());
 
+        // Calculate net salary
+        double netSalary = emp1.calculateNetSalary(40000, 0.3, 4000);
+        System.out.println("Net salary: " + netSalary);
 
+        // Calculate employee pension
+        int pension = EmployeeInfo.calculateEmployeePension();
+        System.out.println("Employee pension: " + pension);
 
+        // Get the number of years
+        int years = EmployeeInfo.getNumberOfYears("May,2011", "August,2023");
+        System.out.println("Number of years: " + years);
 
+        // Create an instance of EmployeeInfo using default constructor
+        EmployeeInfo employeeInf = new EmployeeInfo();
 
+        // Calculate salary and benefits
+        System.out.println("Salary: " + employeeInf.calculateSalary());
+        System.out.println("Benefits: " + employeeInf.benefits());
+
+        // Create an instance of EmployeeInfo with ID only
+        EmployeeInfo employeeA = new EmployeeInfo(7855);
+        System.out.println("Employee ID: " + employeeA.employeeId());
+
+        // Create an instance of EmployeeInfo with name only
+        EmployeeInfo employeeB = new EmployeeInfo("Karima");
+        System.out.println("Employee name: " + employeeB.employeeName());
+
+        // Assign department
+        System.out.println("Assigned department: " + employeeInf.assignDepartment());
+
+        // Get the month index
+        int monthIndex = EmployeeInfo.whichMonth("Ma");
+        System.out.println("Month index: " + monthIndex);
     }
-
 }
